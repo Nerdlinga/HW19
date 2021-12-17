c98b1ac4d8670e4ffa3bf6678f5ace9011436454
Nerdlinga <lorielizabethdyer@gmail.com>
2021-12-16 22:14:44 -0600

    close button

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 $( modal ).load(function() {
     //run modal
 
-    $('body').on('click', '#modal', function() { //must use on, as the lightbox element is inserted into the DOM
-        $('#modal').hide();
     });
 
  
-    $('close').on('click', '#modal', function() { //must use on, as the lightbox element is inserted into the DOM
-        $('#modal').hide();
-    });
+    $('body #myBtn span').click(function () {
+        $('#div-to-toggle').hide();
+        event.stopPropagation();
+    })
