1f0d2d44a416408177639aeb9c03297d51b73b76
Nerdlinga <lorielizabethdyer@gmail.com>
2021-12-16 21:15:57 -0600

    did a bunch of jquery

--- index.js
+++ index.js
@@ -1,25 +1,84 @@
-$(document).ready(function() {
-    $('.someClass').handlerMethod();
-});
+$(document).ready(function() { 
+     
+    
+    var id ='#dialog';
+    
+       
+    
+    //Get the screen height and width
+    
+    var maskHeight = $(document).height();
+    
+    var maskWidth = $(window).width();
+    
+       
+    
+    //Set heigth and width to mask to fill up the whole screen
+    
+    $('#mask').css({'width':maskWidth,'height':maskHeight});
+    
+     
+    
+    //transition effect
+    
+    $('#mask').fadeIn(500);
+    
+    $('#mask').fadeTo("slow",0.9); 
+    
+       
+    
+    //Get the window height and width
+    
+    var winH = $(window).height();
+    
+    var winW = $(window).width();
+    
+                   
+    
+    //Set the popup window to center
+    
+    $(id).css('top',  winH/2-$(id).height()/2);
+    
+    $(id).css('left', winW/2-$(id).width()/2);
+    
+       
+    
+    //transition effect
+    
+    $(id).fadeIn(2000);  
+    
+       
+    
+    //if close button is clicked
+    
+    $('.window .close').click(function (e) {
+    
+    //Cancel the link behavior
+    
+    e.preventDefault();
+    
+     
+    
+    $('#mask').hide();
+    
+    $('.window').hide();
+    
+    });
+    
+     
+    
+    //if mask is clicked
+    
+    $('#mask').click(function () {
+    
+    $(this).hide();
+    
+    $('.window').hide();
+    
+    });
+    
+       
+    
+    });
+    
 
-// Get the modal
-var modal = document.getElementById("myModal");
-
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
