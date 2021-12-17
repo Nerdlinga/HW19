cb1361ca4015f0ae5086466f9862cb879e289e46
Nerdlinga <lorielizabethdyer@gmail.com>
2021-12-16 21:49:46 -0600

    more js

--- index.js
+++ index.js
@@ -1,4 +1,20 @@
+$(modal).ready(function() {
+    // Handler for .ready() called.
+    /* Cache the popup. */
+  var modal = document.getElementById("modal");
 
+  /* Show the popup. */
+  modal.classList.remove("hidden");
+
+  /* Fade the popup in */
+  setTimeout(()=>modal.classList.add("fade-in"));
+
+    /* Fade the popup out */
+    modal.classList.remove("fade-in");
+    /* Hide the popup. */
+    setTimeout(()=>modal.classList.add("hidden"), 300);
+
+  });
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
