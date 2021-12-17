4ed78c53971ddff43ab8c212990868bf5ddf05a1
Nerdlinga <lorielizabethdyer@gmail.com>
2021-12-16 22:02:39 -0600

    close modal

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 $( modal ).load(function() {
     //run modal
-  });
 
+    $('body').on('click', '#modal', function() { //must use on, as the lightbox element is inserted into the DOM
+        $('#modal').hide();
+    });
+
+ 
+    $('close').on('click', '#modal', function() { //must use on, as the lightbox element is inserted into the DOM
+        $('#modal').hide();
+    });
